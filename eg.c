259d2bc0d11197c3fcf9eab7928f3ade3ee91427
kleanupguy7 <panos.pan92@gmail.com>
2018-10-20 17:34:22 +0300

    First main attempt

---
+++ eg.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+	int a[3][3] = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};
+	int b[3][3] = {{5, 8, 1}, {2, 10, 2}, {1, 0, 3}};
+
+
+	int aRows,bRows;
+	aRows = 3;
+	bRows = 3;
+
+	int n = 2;
+	char mask = 0xFF;
+	
+
+
+
+}
