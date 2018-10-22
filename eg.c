cdb2f60d9d50afd110f54fabe569dc48e382a151
nickolsv <nickols1234@gmail.com>
2018-10-22 13:43:57 +0300

    Minimal Changes

--- eg.c
+++ eg.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 #include "structs.h"
 
-#define N 3
+#define N 4
 
 
 
 int main(void){
-	int32_t a[3][2] = {{1,1},{1,5},{10,1}};
+	int32_t a[3][2] = {{15,1},{31,5},{10,1}};
 	int32_t b[3][2] = {{1,1},{1,5},{10,1}};
 
 	int32_t bits = (1 << N);
@@ -18,7 +18,6 @@ int main(void){
 
 	int32_t  mask = (1 << N) - 1;
 
-	// printf("%x %x\n", );
 
 	int32_t reorderedArray[3][3];
 
@@ -31,6 +30,7 @@ int main(void){
 
 	for(int i = 0 ; i < rows ; i++){
 		LSB = a[i][0] & mask;
+		printf(" Oreganol: %x Last Bits: %x\n",a[i][0],LSB);
 		histogram[LSB]++;
 	}
 
