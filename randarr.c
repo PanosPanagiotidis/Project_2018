bc3cf1beff46e639d6f6ac3862e2066a9cb1ead8
GeorgeAmp <g.ampatzidis2@gmail.com>
2018-10-22 14:46:10 +0300

    Typedef structs,return structs as results

--- randarr.c
+++ randarr.c
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
-{
+typedef struct dataInits{
+	int rows;
+	int cols;
+	int** array;
+}dataInits;
+
+
+dataInits* create_random_array(){
+	dataInits *data = malloc(sizeof(dataInits));
+
+
 	time_t t;
 	int **arr;
 	srand((unsigned) time(&t));
+
+
 
 	int r = rand()%100 + 1;
 	int c = rand()%100 + 2;
@@ -19,4 +30,24 @@ int main(void)
      for (int i = 0; i < r; i++) 
         for (int j = 0; j < c; j++) 
             arr[i][j] = rand()%300;
+
+    data->rows = r;
+    data->cols = c;
+    data->array = arr;
+    return data;
+}
+
+
+
+int main(void)
+{
+	dataInits *A = create_random_array();
+	printf("A data is | rows = %d | cols = %d | \n",A->rows,A->cols);
+	for (int i = 0 ; i < A->rows ; i++){
+		for(int j = 0 ; j < A->cols ; j++){
+			printf("%d ",A->array[i][j]);
+		}
+		printf("\n");
+	}
+	dataInits *B = create_random_array();
 }
