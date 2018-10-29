34868bb5f0d25ae48a86ad369984a6728b56bfdd
nickolsv <nickols1234@gmail.com>
2018-10-29 17:02:16 +0200

    Create daindex structs

---
+++ daindex.c
@@ -0,0 +1,1 @@
+#include "daindex.h"
--- daindex.h
+++ daindex.h
@@ -1,7 +1,22 @@
 #ifndef __DA_INDEX_H__
 #define __DA_INDEX_H__
 
+#define HASH_RANGE 101
 
+typedef struct bucketHashTable
+{
+
+} bucketHashTable;
+
+typedef struct chainArray {
+
+} chainArray;
+
+typedef struct daIndex
+{
+    bucketHashTable bucket[HASH_RANGE];
+    chainArray *chain;
+} daIndex;
 
 
 
