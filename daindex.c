e89b29d14e36df4e6b60bb1ec7232ed0c5330456
nickolsv <nickols1234@gmail.com>
2018-10-29 18:28:15 +0200

    daindex progress

--- daindex.c
+++ daindex.c
@@ -1,1 +1,33 @@
+#include <stdlib.h>
 #include "daindex.h"
+#include "structs.h"
+
+daIndex **createDAIndexArray(bucket_array *bckArray)							// Creates and returns an index for each bucket in bckArray
+{
+	int bckCount = bckArray->size;
+	daIndex **indArray = malloc(bckCount*sizeof(daIndex *));
+
+	for(int i=0; i<bckCount; i++)												// Create each index
+		indArray[i] = createDAIndex(bckArray->bucketArray[i]);
+
+	return indArray;
+}
+
+daIndex *createDAIndex(bucket *buck)											// Creates and returns and index of a bucket
+{
+	int bckSize = buck->size;
+
+	daIndex* bckIndex = malloc(sizeof(daIndex));
+	bckIndex->chain = malloc(sizeof(chainArray));
+
+	bckIndex->chain->array = malloc(bckSize*sizeof(int));						// Allocating memory for chain array
+	bckIndex->chain->size = bckSize;											// Chain Array has size equal to the bucket
+
+	for(int i=0; i<bckSize; i++ )
+	{
+		
+	}
+
+
+	return 0;
+}
--- daindex.h
+++ daindex.h
@@ -1,25 +1,31 @@
 #ifndef __DA_INDEX_H__
 #define __DA_INDEX_H__
 
+#include <stdint.h>
+#include "structs.h"
+
 #define HASH_RANGE 101
 
-typedef struct bucketHashTable
+typedef struct bucketHashTableData
 {
-
-} bucketHashTable;
+	int position;								// Position of start of chain in chainArray
+	int32_t unhashedData;						// Copy of unhashed key for collision handling
+} bucketHashTableData;
 
 typedef struct chainArray {
-
+	int size;
+	int *array;
 } chainArray;
 
 typedef struct daIndex
 {
-    bucketHashTable bucket[HASH_RANGE];
-    chainArray *chain;
+	bucketHashTableData bucket[HASH_RANGE];
+	chainArray *chain;
 } daIndex;
 
 
-
-
+int bucketHashFunction(int32_t);
+daIndex **createDAIndexArray(bucket_array *);
+daIndex *createDAIndex(bucket *);
 
 #endif
