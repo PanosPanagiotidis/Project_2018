ad3922f992cc084f2001f2428a238dd2b4135e88
nickolsv <nickols1234@gmail.com>
2018-11-19 23:37:04 +0200

    Change Order of the Insertion of a Relation to Struct

--- src/relation_loader.c
+++ src/relation_loader.c
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 void init_relations(const char* filepath)
-{	
+{
 	int docs = 0;
 	FILE *fp;
 	size_t len = 0;
@@ -54,7 +54,7 @@ void load_relations(const char* filename)
 	int fd = open(filename,O_RDONLY);
 	if(fd == -1)
 		fprintf(stderr,"Could not open requested file \n");
-	
+
 
 	//Get file size
 
@@ -66,28 +66,39 @@ void load_relations(const char* filename)
 	long length = sb.st_size;
 	char* addr = (char*)(mmap(0,length,PROT_READ,MAP_PRIVATE | MAP_POPULATE,fd,0));
 
-	if(addr == MAP_FAILED)
+	char* tmpaddr = addr;
+
+	if(tmpaddr == MAP_FAILED)
 		fprintf(stderr,"Cannot mmap %s of length %ld\n",filename,length);
 
 	if(length < 16)
 		fprintf(stderr,"Relation file %s does not contain a valid header\n",filename);
 
+	printf("%p\n",tmpaddr);
 
-	u_int64_t size = *((u_int64_t*)addr);
-	addr+=sizeof(size);
-	u_int64_t num_columns =*((u_int64_t*)addr);
-	
+	u_int64_t size = *((u_int64_t*)tmpaddr);
+	tmpaddr+=sizeof(size);
+	u_int64_t num_columns =*((u_int64_t*)tmpaddr);
+	tmpaddr = tmpaddr + sizeof(u_int64_t);
 
 	printf("rows is %ld and columns is %ld\n",size,num_columns);
 
 
-	for(int i = 0 ; i < num_columns ; i++){
-		for(int j = 0; j < size; j++)
+	for(int i = 0 ; i < size ; i++)
+	{
+		for(int j = 0 ; j < num_columns ; j++)
 		{
-			addr+=sizeof(u_int64_t);
-			printf("%ld ", *((u_int64_t*)addr 	));
-			printf("|");
+			printf("%ld |", *((u_int64_t*)tmpaddr) );
+
+			// DO THE THING
+
+			tmpaddr+=size*sizeof(u_int64_t);
 		}
 		printf("\n");
+		tmpaddr = tmpaddr - 3*size*sizeof(u_int64_t);
+		tmpaddr = tmpaddr + sizeof(u_int64_t);
 	}
+
+	munmap(addr,length);
+
 }
